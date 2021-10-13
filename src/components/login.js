eac8d03ebccd3ee2057eb22269b46e104c3dc4bc
ovan zaimul <ovanzaimul@gmail.com>
2021-10-13 12:48:28 +0800

    Add and style login header element

---
+++ src/components/login.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+
+import React from 'react';
+
+const login = () => {
+  return (
+    <Container>
+      <Nav>
+        <a href='/'>
+          <img src='/images/login-logo.svg' alt='' />
+        </a>
+        <div>
+          <Join>Join Now</Join>
+          <SignIn>Sign in</SignIn>
+        </div>
+      </Nav>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  padding: 0px;
+`;
+
+const Nav = styled.nav`
+  max-width: 1128px;
+  margin: auto;
+  padding: 12px 0 16px;
+  display: flex;
+  align-items: center;
+  position: relative;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  & > a {
+    width: 135px;
+    height: 34px;
+    @media (max-width: 768px) {
+      padding: 0 5px;
+    }
+  }
+`;
+
+const Join = styled.a`
+  font-size: 16px;
+  padding: 10px 12px;
+  color: rgba(0, 0, 0, 0.6);
+  border-radius: 4px;
+  margin-right: 12px;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+    color: rgba(0, 0, 0, 0.9);
+    text-decoration: none;
+  }
+`;
+
+const SignIn = styled.a`
+  box-shadow: inset 0 0 0 1px #0a66c2;
+  color: #0a66c2;
+  border-radius: 24px;
+  transition-duration: 167ms;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 40px;
+  padding: 10px 24px;
+  text-align: center;
+  background-color: rgba(0, 0, 0, 0);
+  &:hover {
+    background-color: rgba(112, 181, 249, 0.15);
+    text-decoration: none;
+  }
+`;
+
+export default login;
